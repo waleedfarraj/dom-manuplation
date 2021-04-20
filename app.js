a54ea27377cc528d72582ee94e7f9156f38f321c
waleedfarraj <waleed.farraj95@gmail.com>
2021-04-21 01:16:35 +0300

    fixed naming convection

--- app.js
+++ app.js
@@ -26,16 +26,16 @@ function createTable()
     deleteTable()
     
    
-let rn = window.prompt("Input number of rows", 1);
-let cn = window.prompt("Input number of columns",1);
+let rowNumber = prompt("Input number of rows", 1);
+let columnNumber = prompt("Input number of columns",1);
   
- for(var r=0;r<parseInt(rn,10);r++)
+ for(let r=0;r<parseInt(rowNumber,10);r++)
   {
-   var x=document.getElementById('myTable').insertRow(r);
-   for(var c=0;c<parseInt(cn,10);c++)  
+   let table=document.getElementById('myTable').insertRow(r);
+   for(let c=0;c<parseInt(columnNumber,10);c++)  
     {
-     var y=  x.insertCell(c);
-     y.innerHTML="Row-"+r+" Column-"+c; 
+     let cell=  table.insertCell(c);
+     cell.innerHTML="Row-"+r+" Column-"+c; 
     }
    }
 }
